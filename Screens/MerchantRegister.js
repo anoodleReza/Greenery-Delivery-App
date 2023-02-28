382b9f4f0b0124a12fd679fc9e25b3933cc2bae3
NoodleReza <reza.brilliansah77@gmail.com>
2023-02-28 11:11:36 +0700

    Register Stuff

--- Screens/MerchantRegister.js
+++ Screens/MerchantRegister.js
@@ -3,41 +3,45 @@ import { StyleSheet, View, Image} from 'react-native';
 import { StackActions } from '@react-navigation/native';
 import { Surface, Button, Text, TextInput } from 'react-native-paper';
 import * as React from 'react';
+import { Formik } from 'formik';
 
 export default function MerchantRegister( {navigation}) {
 
-  const [usernameReg, setUsernameReg] = React.useState('');
-  const [mailReg, setMailReg] = React.useState('');
-  const [passwordReg, setPasswordReg] = React.useState('');
-  const [confirmPasswordReg, setConfirmPasswordReg] = React.useState('');
-
-  const register = () => {
-    Axios.post("https://localhost3001/register", {
-      username: usernameReg, 
-      email: mailReg, 
-      password: passwordReg
-    }).then((response)=>{
-      console.log(response);
-    });
-  };
-
   return (
-    <View style={styles.container}>
-      {/* Image section */}
-      <Image style = {{ width: 250, height: 250 }} source={require('../assets/logo.png')} />
+    <Formik
+      initialValues={{ 
+        username: '', 
+        email: '', 
+        password: '', 
+        confirmPassword:''}
+      }
+      onSubmit={(values)=>{
+        console.log(values);
+        navigation.dispatch(StackActions.replace("MerchantRegDetails"));
+      }}
+      
+    >
+      {({ handleChange,handleBlur, handleSubmit, values }) => (
+      <View style={styles.container}>
+        {/* Image section */}
+        <Image style = {{ width: 250, height: 250 }} source={require('../assets/logo.png')} />
 
-      {/* Top Section */}
+        {/* Input section */}
         <TextInput
           mode="outlined"
           placeholder="Enter Username..."
           style={styles.input}
-          onChange={(e)=> {setUsernameReg(e.target.value)}}
+          onChangeText={handleChange('username')}
+          onBlur={handleBlur('username')}
+          value={values.username}
         />
         <TextInput
           mode="outlined"
           placeholder="Enter email address..."
           style={styles.input}
-          onChange={(e)=> {setMailReg(e.target.value)}}
+          value={values.email}
+          onChangeText={handleChange('email')}
+          onBlur={handleBlur('email')}
         />
         <TextInput
           mode="outlined"
@@ -45,7 +49,9 @@ export default function MerchantRegister( {navigation}) {
           style={styles.input}
           secureTextEntry
           right={<TextInput.Icon name="eye" />}
-          onChange={(e)=> {setPasswordReg(e.target.value)}}
+          value={values.password}
+          onChangeText={handleChange('password')}
+          onBlur={handleBlur('password')}
         />
         <TextInput
           mode="outlined"
@@ -53,30 +59,37 @@ export default function MerchantRegister( {navigation}) {
           style={styles.input}
           secureTextEntry
           right={<TextInput.Icon name="eye" />}
-          onChange={(e)=> {setConfirmPasswordReg(e.target.value)}}
+          value={values.confirmPassword}
+          onChangeText={handleChange('confirmPassword')}
+          onBlur={handleBlur('confirmPassword')}
         />
 
-      {/* Bottom Buttons */}
-      <Button         
-        style={styles.buttonDefault}
-        textColor='black'
-        mode="contained" 
-        onPress={() => {
-          register
-          navigation.dispatch(StackActions.replace("MerchantRegDetails"));
-        }}
-        >
-        Sign Up
-      </Button>
-      <Text>
-        <Text>Already have an account? Sign In </Text>
-        <Text style={styles.Highlight} onPress={() => {navigation.dispatch(StackActions.replace("MerchantSignIn"));}}>
-          Here
-        </Text>         
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+        {/* Bottom Buttons */}
+        <Button         
+          style={styles.buttonDefault}
+          textColor='black'
+          mode="contained" 
+          onPress={handleSubmit}
+          // onPress={() => {navigation.dispatch(StackActions.replace("MerchantRegDetails"));}}
+          >
+          Sign Up
+        </Button>
+
+        <Text>
+          <Text>Already have an account? Sign In </Text>
+          <Text style={styles.Highlight} onPress={() => {navigation.dispatch(StackActions.replace("MerchantSignIn"));}}>
+            Here
+          </Text>         
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    )}
+    </Formik>
   );
+}
+
+const submission = () => {
+  
 }
 
 const styles = StyleSheet.create({
@@ -105,3 +118,8 @@ const styles = StyleSheet.create({
   },
 
 });
+
+//register backend
+const handleLogin = (credentials) => {
+  co
+}
